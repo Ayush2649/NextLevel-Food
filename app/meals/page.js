155fb1581b8c889d33ef0ef36c56c2181ea295fb
Ayush Sahu <ayush.sahu@contentstack.com>
2026-01-13 07:13:50 +0530

    Add meal item styles, initialize database, and implement meal retrieval
    - Created a new CSS module for meal item styling to enhance UI.
    - Added an initialization script (initdb.js) to populate the meals database with dummy data.
    - Implemented a function (getAllMeals) to retrieve all meals from the database.
    - Updated package.json and package-lock.json to include better-sqlite3 as a dependency.
    - Added a new SQLite database file (meals.db) to store meal data.

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,28 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "../../components/meals/meal-grid";
+import getAllMeals from "../../lib/meals";
 
-export default function Meals() {
-    return <main>
-        <h1>This is the Meals section</h1>
-        <p><Link href="/meals/meal-1"> Meal 1 </Link></p>
-        <p><Link href="/meals/meal-2"> Meal 2 </Link></p>
-    </main>
+export default async function MealsSharePage() {
+    const meals = await getAllMeals();
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is easy and
+          fun!!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your Favourite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} /> 
+      </main>
+    </>
+  );
 }
