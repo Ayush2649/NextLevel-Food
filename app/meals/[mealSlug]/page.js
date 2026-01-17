e78c949b801ccf8f8ff95077e5ce367174ec53b9
Ayush Sahu <ayush.sahu@contentstack.com>
2026-01-17 09:40:07 +0530

    Implement meal details page with dynamic routing and styling; add meal retrieval logic

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,9 +1,41 @@
-export default async function MealsSlug({params}) {
-    const {mealSlug} = await params;
-    return (
-        <main>
-            <h1>This is the dynamic route for meal</h1>
-            <p>{mealSlug}</p>
-        </main>
-    )
+import { getMealBySlug } from "@/lib/meals";
+import { notFound } from "next/navigation";
+import Image from "next/image";
+import classes from "./page.module.css";
+
+export default async function MealsSlug({ params }) {
+  const { mealSlug } = await params; // ✅ unwrap params
+
+  const meal = getMealBySlug(mealSlug);
+
+  meal.instructions = meal.instructions.replaceAll("\n", "<br/>");
+
+  if (!meal) {
+    notFound();
+  }
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill src={meal.image} alt={meal.title} />
+        </div>
+
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        />
+      </main>
+    </>
+  );
 }
