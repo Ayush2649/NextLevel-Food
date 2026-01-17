49ee87473d2eaf6e1c4077be1623cf8bb5e82b76
Ayush Sahu <ayush.sahu@contentstack.com>
2026-01-17 12:07:38 +0530

    Implement Share Meal Page with form for meal sharing and image upload functionality

---
+++ components/meals/image-picker.js
@@ -0,0 +1,58 @@
+"use client";
+
+import { useRef, useState } from "react";
+import classes from "./image-picker.module.css";
+import Image from "next/image";
+
+export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
+  const imageInputRef = useRef();
+
+  function pickImageHandler() {
+    imageInputRef.current.click();
+  }
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  }
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && <Image src={pickedImage} alt="Picked" fill />}
+        </div>
+        <input
+          type="file"
+          id={name}
+          name={name}
+          accept="image/*"
+          className={classes.input}
+          ref={imageInputRef}
+          onChange={handleImageChange}
+          required
+        />
+        <button
+          type="button"
+          className={classes.button}
+          onClick={pickImageHandler}
+        >
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+}
