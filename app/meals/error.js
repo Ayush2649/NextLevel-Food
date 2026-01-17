0231919c09f27e38b89237cd7ba0e3c976d6078e
Ayush Sahu <ayush.sahu@contentstack.com>
2026-01-17 09:09:17 +0530

    Add error and not found components; update meal retrieval function

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Error(){
+    return (
+        <main className="error">
+            <h1>AN ERROR OCCURRED!</h1>
+            <p>Could not fetch meals. Please try again later.</p>
+        </main>
+    )
+}
